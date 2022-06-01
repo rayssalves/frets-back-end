4cd6adbfe8e67593975892c1e504259b388fe938
rayssalves <rayssa.a.vieira@outlook.com>
2022-06-01 15:24:08 +0200

    sockect connection

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-// const { Router } = require("express");
 const express = require("express");
-const cors = require("cors");
-const { createServer } = require("http");
-const ownersRouter = require("./routers/owners");
-
 const app = express();
-// const router = new Router();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
 
 app.use(cors());
-app.use(express.json());
-app.use("/owners", ownersRouter);
 
+const server = http.createServer(app);
 const PORT = 4000;
-//socket connection
-const httpServer = createServer();
-const io = require("socket.io")(httpServer, {
+const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
     methods: ["GET", "POST"],
@@ -22,11 +16,62 @@ const io = require("socket.io")(httpServer, {
     credentials: true,
   },
 });
-
+//CONNECTION
 io.on("connection", (socket) => {
-  console.log("made socket connection", socket.id);
+  console.log(`Made socket connection: ${socket.id}`);
+  //ROOM
+  socket.on("join_room", (data) => {
+    socket.join(data);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
 });
 
-httpServer.listen(PORT, () => {
-  console.log("server is running on ", PORT);
+server.listen(PORT, () => {
+  console.log("SERVER IS RUNNING on", PORT);
 });
+
+// const express = require("express");
+// const cors = require("cors");
+// const { createServer } = require("http");
+// const { Server } = require("socket.io");
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// const PORT = 4000;
+// //socket connection
+// const httpServer = createServer();
+
+// const io = require("socket.io")(httpServer, {
+//   cors: {
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST"],
+//     allowedHeaders: ["instant-chat-header"],
+//     credentials: true,
+//   },
+// });
+
+// io.on("connection", (socket) => {
+//   console.log("made socket connection", socket.id);
+//   socket.on("message", (arg) => {
+//     console.log(arg);
+//   });
+//   // socket.send(<channel>, <content>)
+// });
+// //room
+// socket.on("join_room", (data) => {
+//   socket.join(data);
+// });
+
+// socket.on("send_message", (data) => {
+//   socket.to(data.room).emit("receive_message", data);
+// });
+
+// httpServer.listen(PORT, () => {
+//   console.log("server is running on ", PORT);
+// });
